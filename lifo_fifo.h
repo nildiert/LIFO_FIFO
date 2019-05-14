c2df358012755e4c734417dd201bcfa4798def9c
nildiert <niljordan23@gmail.com>
2019-05-14 19:17:37 +0000

    [FIX] Fxed the header file

--- lifo_fifo.h
+++ lifo_fifo.h
@@ -2,20 +2,33 @@
 #define LIFO_FIFO_H
 
 /**
-* struct process_s - Process
-* @id: id
-* @prev: points to the previous node
-* @next: points to the next node
-*
-* Description: Queue
-* for Holberton project
-*/
-
-typedef struct process_s
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
 {
-	int id;
-	struct process_t *prev;
-	struct process_t *next;
-} process_t;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
 
 #endif /* LIFO_FIFO_H */

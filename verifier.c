5a026a8861dd41fa2ada82fe6da4165b49cb4d8c
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-14 23:06:59 +0000

    [Pseudo-FT] Diagram on pseudo code the verifier

---
+++ verifier.c
@@ -0,0 +1,20 @@
+#include "lifo_fifo.h"
+/**
+ *verifier - Verify coincidence on a structure predefined
+ *Return: If coincidence 0, else 1
+ */
+void verifier(char **reception)
+{
+/* Verificar las posiciones
+El primer elemento del array [0] en una estructura, debe compararse
+(Ver calc)
+Si coincide, retornar 0.
+Si no coincide, retornar 1.
+Argumento dos [1]: Si existe, crear un nodo en la lista de la estructura (completar)
+*/
+	if (argc != 2)
+	{
+
+	}
+
+}

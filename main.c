0b3d39037b3576b21b3994cb3500627603787998
nildiert <niljordan23@gmail.com>
2019-05-15 20:27:38 +0000

    [FIX] Some things in strtok function

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main(int argc, char **argv)
         int fd = open(argv[1], O_RDONLY);
         int size= 1024, count = 0;
 	FILE *file = fopen ( argv[1], "r" );
+	char **arguments;
 
 	(void)argc;
 	(void)argv;
@@ -18,13 +19,13 @@ int main(int argc, char **argv)
 	if ( file != NULL )
 	{
 		char line [ 128 ]; /* or other suitable maximum line size */
-		while ( fgets ( line, sizeof line, file ) != NULL ) 
+		while ( fgets ( line, sizeof line, file ) != NULL )
 		{
 			count++;
-			printf("%s count: ", line);
+			arguments = _strtok(line);
+			printf("Main::[ %s ][ %s ] Strtok Arg\n", arguments[0], arguments[1]);
 		}
 		fclose ( file );
 	}
-	printf("File: %s fd: %d count: %d\n", argv[1], fd, count);
 	return (0);
 }

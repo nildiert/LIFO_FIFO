12f08a556b9298313e5193dca09362f6615feaa5
Mariana <marianaplazas@live.com>
2019-05-16 14:46:57 +0000

    [feature] function nop pop ad pint, first version

---
+++ _pint.c
@@ -0,0 +1,18 @@
+#include "fifo_lifo.h"
+/**
+ *
+ *
+ */
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head;
+
+	head = *stack;
+	if (head == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", head->n);
+}
---
+++ _pop.c
@@ -0,0 +1,20 @@
+#include "fifo_lifo"
+/**
+ *
+ *
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head;
+	head = *stack;
+
+	if (head == NULL)
+	{
+		printf("L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	head = (*stack)->next;
+	free(*stack);
+	*stack = head;
+}

6427faae4f5e908d9787f68bcbade0c5e8a70aff
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-16 20:51:18 +0000

    Useless

--- _add.c
+++ _add.c
@@ -1,6 +1,7 @@
 #include "lifo_fifo.h"
 /**
- *get_verifier - Gets the right function to the monty op string
- *@argue: Operator received, based on the Read file
- *Return: Pointer to the right function
+ *
+ *
+ *
+ *
  */

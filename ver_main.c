8b4d93716925c0c701f0494c94aa983dd04fcfd8
GaviriaAmaya <judamayaga@unal.edu.co>
2019-05-16 14:50:39 +0000

    [Pulling]

--- ver_main.c
+++ ver_main.c
@@ -1,7 +1,22 @@
-#include "monty.h"
+#include "lifo_fifo.h"
 /**
- *
- *
- *
- *
+ *main - Verifier of the get_verif
+ *Return: Always success
  */
+int main(void)
+{
+	int v, w, x;
+	char s[] = "push";
+	char t[] = "nop";
+	char u[] = "poll";
+
+	v = get_verifier(s);
+	w = get_verifier(t);
+	x = get_verifier(u);
+
+	printf("%d", v);
+	printf("%d", w);
+	printf("%d", x);
+
+	return(0);
+}

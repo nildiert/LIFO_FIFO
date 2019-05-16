676ab8387a37bca579acb1da93834be2821f7823
David Latorre <david@latorredev.com>
2019-05-16 16:58:22 -0500

    [Update] Add handle error if unknow op is passed

--- verifier.c
+++ verifier.c
@@ -1,27 +1,25 @@
 #include "lifo_fifo.h"
 /**
  *verifier - Verify coincidence on a structure predefined
+ *@reception: entry argument
+ *@stack: entry argument
+ *@line: entry argument
  *Return: If coincidence 0, else 1
  */
 void verifier(char **reception, stack_t **stack, int line)
 {
-/* Verificar las posiciones
-El primer elemento del array [0] en una estructura, debe compararse
-(Ver calc)
-Si coincide, retornar 0.
-Si no coincide, retornar 1.
-Argumento dos [1]: Si existe, crear un nodo en la lista de la estructura (completar)
-*/
 	int j = 0;
 	/* int global; */
 	instruction_t instructions[] = {
 		{"push", _push},
 		{"pall", _pall},
-		/*	{"pint", _pint},
-		{"pop", _pop},
-		{"swap", _swap},
-		{"add", _add},
-		{"nop", _nop},*/
+		/*
+		* {"pint", _pint},
+		* {"pop", _pop},
+		* {"swap", _swap},
+		* {"add", _add},
+		* {"nop", _nop},
+		*/
 		{NULL, NULL}
 	};
 
@@ -34,17 +32,16 @@ Argumento dos [1]: Si existe, crear un nodo en la lista de la estructura (comple
 	for (j = 0; instructions[j].opcode; j++)
 	{
 
-		if (!(strcmp(instructions[j].opcode,reception[0])))
+		if (!(strcmp(instructions[j].opcode, reception[0])))
 		{
 			instructions[j].f(stack, (unsigned int)line);
 			break;
 		}
 	}
+		if (instructions[j].f == NULL)
+		{
+			printf("L %d: unknown instruction %s\n", line, reception[0]);
+			exit(EXIT_FAILURE);
+		}
 
-	
-/*	for (j = 0; instructions[j].opcode; j++).
-	{
-		if (reception[0] == *(instructions[j].opcode))
-			printf("%s", reception[0]);
-			}*/
 }
